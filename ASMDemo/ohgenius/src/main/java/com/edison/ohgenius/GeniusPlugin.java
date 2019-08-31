402ada06d83e39d81d74d2a33fe1b4ec998e15cc
liaoshouwang <Edison.liao@cootek.cn>
2019-08-31 10:50:33 +0800

    edison- demo搭建初步

---
+++ ASMDemo/ohgenius/src/main/java/com/edison/ohgenius/GeniusPlugin.java
@@ -0,0 +1,16 @@
+package com.edison.ohgenius;
+
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+/**
+ * created by edison 2019-08-31
+ */
+public class GeniusPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+
+    }
+}
